abb0095397a518d5f2d791c722177be1b1cbacb3
Ricardo Glez <ricardogonzaleznavarro95@gmail.com>
2023-07-05 16:34:38 -0600

    queryoptions

--- src/data/suggestions.ts
+++ src/data/suggestions.ts
@@ -1,17 +1,24 @@
+import { QueryOptionsType } from '@/types'
+import { SuggestionsResponse, suggestionsPagination } from '@/types/suggestions'
+import { UserPagination } from '@/types/users'
 import { useQuery } from 'react-query'
-
 import { API_ENDPOINTS } from './client/api-endpoints'
-import { suggestionsClient } from './client/suggestions'
+import { mapPaginatorData } from '@/utils/data-mappers'
+import { suggestionClient } from './client/suggestions'
 
-export const useSuggestionsQuery = () => {
-  const { data, error, isLoading } = useQuery(
-    API_ENDPOINTS.SUGGESTIONS,
-    suggestionsClient.suggestions
+export const useSuggestionQuery = (params: Partial<QueryOptionsType>) => {
+  const { data, isLoading, error } = useQuery<suggestionsPagination, Error>(
+    [API_ENDPOINTS.USERS, params],
+    () => suggestionClient.fetchsuggestions(params),
+    {
+      keepPreviousData: true,
+    }
   )
 
   return {
-    suggestions: data ?? [],
-    error,
+    suggestions: data?.suggestions as [],
     loading: isLoading,
+    paginatorInfo: mapPaginatorData(data as any),
+    error,
   }
 }
