b2e78d65b2a1b00fc8af894ac46fbcef9887d033
Sebastian <sebastian.inzunza@sissadigital.com>
2023-10-19 12:42:58 -0600

    feat: :bug: fix for production

---
+++ src/pages/menu/[id]/index.tsx
@@ -0,0 +1,16 @@
+import MenuForm from '@/components/Menu/menu-form'
+import Layout from '@/components/layout/admin'
+
+export default function CreateMenu() {
+  return (
+    <>
+      <div className="flex border-b border-dashed border-border-base py-5 sm:py-8">
+        <h1 className="text-lg font-semibold text-heading">Crear Menu</h1>
+      </div>
+
+      <MenuForm />
+    </>
+  )
+}
+
+CreateMenu.Layout = Layout
---
+++ src/pages/presentadores/[id]/index.tsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+type Props = {}
+
+type State = {}
+
+class index extends Component<Props, State> {
+  state = {}
+
+  render() {
+    return <div>index</div>
+  }
+}
+
+export default index
