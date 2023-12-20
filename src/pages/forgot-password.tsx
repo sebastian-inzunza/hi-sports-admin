bda352cdb02d3f4069208494f986e5785cc480a6
Sebastian <sebastian_a9@hotmail.com>
2023-12-20 14:36:03 -0600

    feat: test

--- src/pages/forgot-password.tsx
+++ src/pages/forgot-password.tsx
@@ -1,40 +1,41 @@
-import { GetServerSideProps } from "next";
-import { useTranslation } from "next-i18next";
-import { serverSideTranslations } from "next-i18next/serverSideTranslations";
+import { GetServerSideProps } from 'next'
+import { useTranslation } from 'next-i18next'
+import { serverSideTranslations } from 'next-i18next/serverSideTranslations'
 
-import ForgotPasswordForm from "@/components/auth/forget-password/forget-password";
-import AuthPageLayout from "@/components/layout/auth-layout";
-import { SUPER_ADMIN } from "@/utils/constants";
-import { parseContextCookie } from "@/utils/parse-cookie";
-import { Routes } from "@/config/routes";
+import ForgotPasswordForm from '@/components/auth/forget-password/forget-password'
+import AuthPageLayout from '@/components/layout/auth-layout'
+import { SUPER_ADMIN } from '@/utils/constants'
+import { parseContextCookie } from '@/utils/parse-cookie'
+import { Routes } from '@/config/routes'
+//Hola
 
 export const getServerSideProps: GetServerSideProps = async ({
   context,
   locale,
 }: any) => {
-  const cookies = parseContextCookie(context?.req?.headers?.cookie);
+  const cookies = parseContextCookie(context?.req?.headers?.cookie)
   if (cookies?.auth_token) {
     if (cookies?.auth_permissions?.includes(SUPER_ADMIN)) {
       return {
         redirect: { destination: Routes.dashboard, permanent: false },
-      };
+      }
     }
   }
   return {
     props: {
-      ...(await serverSideTranslations(locale!, ["common", "form"])),
+      ...(await serverSideTranslations(locale!, ['common', 'form'])),
     },
-  };
-};
+  }
+}
 
 export default function ForgotPasswordPage() {
-  const { t } = useTranslation();
+  const { t } = useTranslation()
   return (
     <AuthPageLayout>
       <h3 className="mb-6 mt-4 text-center text-base italic text-body">
-        {t("form:form-title-forgot-password")}
+        {t('form:form-title-forgot-password')}
       </h3>
       <ForgotPasswordForm />
     </AuthPageLayout>
-  );
+  )
 }
